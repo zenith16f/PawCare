5ca41f6e7b54af06fa6f9b2ffaef6aa9b5277449
Oiox466 <ledesma.hernandez.leonardo6@gmail.com>
2023-05-27 21:53:37 -0700

    adición de razas en el registro de mascotas

--- src/main/webapp/JS/scriptregistromascota.js
+++ src/main/webapp/JS/scriptregistromascota.js
@@ -12,66 +12,36 @@ function validateText(event) {
     } else {
         return false;
     }
+}  
+
+function validateLen(event) {
+    let nameInput = event.target.value;
+    if (nameInput.length >= 255) {
+        event.preventDefault(); // Evitar que se ingrese más caracteres
+        return false;
+    }
 }
-    
-  function resetForm() {
+
+function resetForm() {
       let form = document.getElementById('form');
       form.reset();
-  }
-  
-  function submit() {
-      let form = document.getElementById('form');
-      let inputs = form.querySelectorAll('input[type="text"], input[type="password"], input[type="email"]');
-      
-      for (let i = 0; i < inputs.length; i++) {
-          if (inputs[i].value.trim() === '') {
-              alert('Por favor, complete todos los campos.');
-              return false;
-          }
-      }
-  
-      let passwordInput = document.getElementById('password');
-      let confirmPasswordInput = document.getElementById('confirmPassword');
-  
-      if (passwordInput.value !== confirmPasswordInput.value) {
-          alert('La contraseña y la confirmación de contraseña deben ser iguales.');
-          return false;
-      }
-  
-      let inputname = document.getElementById("nombre");
-      let inputappat = document.getElementById("appat");
-      let inputapmat = document.getElementById("apmat")
-      let inputdir = document.getElementById("dir")
-      let inputmail = document.getElementById("mail")
-      let inputpass = document.getElementById("password")
-      let inputconf = document.getElementById("confirmPassword")
-  
-      if(inputname.value.length < 3 || inputname.value.length > 255  ||
-          inputappat.value.length < 3 || inputappat.value.length > 255 ||
-          inputapmat.value.length < 3 || inputapmat.value.length > 255 ||
-          inputdir.value.length < 10 || inputdir.value.length > 255 ||
-          inputmail.value.length < 10 || inputmail.value.length > 255 ||
-          inputpass.value.length < 8 || inputpass.value.length > 255 ||
-          inputconf.value.length < 8 || inputconf.value.length > 255){
-          alert("Error, la cantidad de caracteres permitidos en una entrada no se encuentra en el rango permitido");
-          return false
-      }
-      if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(inputmail.value)) {
-          alert("Correo Inválido");
-          return false;
-      }
-      if (!/^([A-Za-záéíóúüñ\s]+)([\s,\.])(\w+\s)?(\d+)?([\s,\.])([A-Za-záéíóúüñ\s]+)$/.test(inputdir.value)) {
-          alert("Dirección Inválida");
-          return false;
-      } 
-      if(inputname.value.trim() == "" || inputapmat.value.trim() == "" || inputappat.value.trim() == ""){
-          alert("Error, no se permiten espacios vacíos en las entradas");
-          return false;
-      }
-      form.submit();
-  }
-  
-  function setFormMargin() {
+}
+
+function submit() {
+    let form = document.getElementById('form');
+    let inputs = form.querySelectorAll('input[type="date"], input[type="text"]');
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.trim() === '') {
+            alert('Por favor, complete todos los campos.');
+            return false;
+        }
+    }
+
+    form.submit();
+}
+
+function setFormMargin() {
       var formDiv = document.querySelector('.form');
       var windowWidth = window.innerWidth;
     
@@ -80,7 +50,7 @@ function validateText(event) {
       } else {
         formDiv.style.marginTop = '150px';
       }
-    }
+}
     
     // Llamamos a la función cuando se carga la página y cuando se cambia el tamaño de la ventana
     window.onload = setFormMargin;
